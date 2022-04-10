d7cef916d6aae3013c995a801a4e5f47278cd72a
Albert Geantă <albertgeanta@gmail.com>
2022-04-10 00:48:41 +0000

    Set target bootanimation res for dot

--- dot_d2x.mk
+++ dot_d2x.mk
@@ -31,6 +31,7 @@ $(call inherit-product, device/samsung/d2x/device.mk)
 TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 3040
 TARGET_SCREEN_WIDTH := 1440
+TARGET_BOOT_ANIMATION_RES := 1440
 
 ## Inherit some common stuff.mk
 $(call inherit-product, vendor/dot/config/common.mk)
